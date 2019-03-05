af8da5fb1ca85b3f2b4a189d9c49a104b3dc30a2
glenn.ludszuweit <glenn.ludszuweit@outlook.com>
2019-03-05 23:35:15 +0100

    Logic Added

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="chat-app">
+    <Conversation :contact="selectContact" :messages="messages"/>
+    <ContactsList :contacts="contacts" @selected="startConversationWith"/>
+  </div>
+</template>
+
+<script>
+import Conversation from "./Conversation";
+import ContactsList from "./ContactsList";
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectContact: null,
+      messages: [],
+      contacts: []
+    };
+  },
+  mounted() {
+    axios.get("/contacts").then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {
+    startConversationWith(contact) {
+      axios.get(`/conversation/${contact.id}`)
+      .then((response) => {
+        this.messages = response.data;
+        this.selectedContact = contact;
+      })
+    }
+  },
+  components: { Conversation, ContactsList }
+};
+</script>
---
+++ resources/js/components/Conversation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="conversation">
+        <h1>{{ contact ? contact.name : 'Select a Contact' }}</h1>
+        <MessagesFeed :contact="contact" :messages="messages" />
+        <MessagesComposer @send="sendMessage" />
+    </div>
+</template>
+
+<script>
+import MessagesFeed from './MessagesFeed';
+import MessagesComposer from './MessagesComposer';
+
+export default {
+    props: {
+        contact: {
+            type: Object,
+            default: null
+        },
+        messages: {
+            type: Array,
+            default: []
+        }
+    },
+    methods: {
+        sendMessage(text) {
+            console.log(text);
+        }
+    },
+    components: {MessagesFeed, MessagesComposer}
+}
+</script>
